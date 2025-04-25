02241d3f39f6003df91cf2085d92e792298d8fec
joshua <joshua.su@huede.com.tw>
2025-04-25 16:04:04 +0800

    +

--- SuParty/MessageQueues/IMessageQueue.cs
+++ SuParty/MessageQueues/IMessageQueue.cs
@@ -1,30 +1,30 @@
-﻿using System;
+﻿//using System;
 
-namespace Huede.FMC2.Communications.MessageQueues
-{
-    /// <summary>
-    /// 定義基於AMQP的訊息佇列的通用介面
-    /// </summary>
-    /// <typeparam name="T">訊息類型</typeparam>
-    public interface IMessageQueue<T> : IDisposable
-    {
-        /// <summary>
-        /// 當從佇列中取得新訊息時，觸發此事件
-        /// </summary>
-        event EventHandler<EventArgs<T>> MessageReceived;
+//namespace Huede.FMC2.Communications.MessageQueues
+//{
+//    /// <summary>
+//    /// 定義基於AMQP的訊息佇列的通用介面
+//    /// </summary>
+//    /// <typeparam name="T">訊息類型</typeparam>
+//    public interface IMessageQueue<T> : IDisposable
+//    {
+//        /// <summary>
+//        /// 當從佇列中取得新訊息時，觸發此事件
+//        /// </summary>
+//        event EventHandler<EventArgs<T>> MessageReceived;
 
-        /// <summary>
-        /// 初始化訊息佇列
-        /// </summary>
-        /// <param name="config">訊息佇列組態</param>
-        /// <returns>成功與否</returns>
-        bool Initialize(Configs.MessageQueueConfig config);
+//        /// <summary>
+//        /// 初始化訊息佇列
+//        /// </summary>
+//        /// <param name="config">訊息佇列組態</param>
+//        /// <returns>成功與否</returns>
+//        bool Initialize(Configs.MessageQueueConfig config);
 
-        /// <summary>
-        /// 發送訊息至佇列
-        /// </summary>
-        /// <param name="message">訊息內容</param>
-        /// <returns>成功與否</returns>
-        bool SendMessage(T message);
-    }
-}
+//        /// <summary>
+//        /// 發送訊息至佇列
+//        /// </summary>
+//        /// <param name="message">訊息內容</param>
+//        /// <returns>成功與否</returns>
+//        bool SendMessage(T message);
+//    }
+//}
