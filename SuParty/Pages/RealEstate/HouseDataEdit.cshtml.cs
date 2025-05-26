fd30255e265a8a37f438ac49f89aba9fb85f87e2
蘇信宇 <joshuahysu@gmail.com>
2025-05-27 01:21:04 +0800

    +

--- SuParty/Pages/RealEstate/HouseDataEdit.cshtml.cs
+++ SuParty/Pages/RealEstate/HouseDataEdit.cshtml.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using SuParty.Data;
-using SuParty.Data.DataModel;
 using SuParty.Data.DataModel.RealEstate;
 using System.Security.Claims;
 
